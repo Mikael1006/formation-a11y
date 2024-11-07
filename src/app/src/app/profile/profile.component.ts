cb225e118c66dd3e095b72f7f058b7a9667b41af
Mikael Moreau <mikael.moreau1006@gmail.com>
2024-11-07 13:14:40 +0100

    input field

--- src/app/src/app/profile/profile.component.ts
+++ src/app/src/app/profile/profile.component.ts
@@ -1,14 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { InputFieldComponent } from '../ui/input-field/input-field.component';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
   imports: [
     CommonModule,
+    InputFieldComponent,
   ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProfileComponent { }
+export class ProfileComponent {}
---
+++ src/app/src/app/ui/input-field/input-field.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, model } from '@angular/core';
+
+let nextId = 0;
+
+@Component({
+  selector: 'app-input-field',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  templateUrl: './input-field.component.html',
+  styleUrl: './input-field.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class InputFieldComponent {
+  id = `input-field-${nextId++}`;
+  value = model('');
+}
